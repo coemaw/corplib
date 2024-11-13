f10d88dd664088bfd9d4c1754bd9890af5d09f95
AK CM <akcm2681@gmail.com>
2024-11-13 16:15:19 +0630

    first initial

---
+++ corpLib/src/main/java/com/sdk/corplib/internet_provider.java
@@ -0,0 +1,11 @@
+package com.sdk.corplib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class internet_provider
+{
+    public static void s(Context t, String message){
+        Toast.makeText(t, message, Toast.LENGTH_SHORT).show();
+    }
+}
